a253f40c1b0aa2859da1e3efba9b0c00e55ced97
longsogood <vietlong685572@gmail.com>
2025-03-24 17:56:32 +0700

    Upload files

---
+++ evaluation.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import requests
+from docx import Document
+
+UNIT_INSIGHTS_EVALUATOR = st.secrets["UNIT_INSIGHTS_EVALUATOR"]
+SUMMARIZED_REPORT_EVALUATOR = st.secrets["SUMMARIZED_REPORT_EVALUATOR"]
+
+# Khởi tạo state nếu chưa tồn tại
+if 'summarized_report_content' not in st.session_state:
+    st.session_state.summarized_report_content = None
+if 'outline_content' not in st.session_state:
+  st.session_state.outline_content = None
+if 'unit_report' not in st.session_state:
+  st.session_state.unit_report = None
+if 'unit_report_insights' not in st.session_state:
+  st.session_state.unit_report_insights = None
+
+tab1, tab2 = st.tabs(["Unit Insights Evaluation", "Summarized Report Evaluation"])
+
+with tab1:
+    st.title("Tải lên báo cáo đơn vị và insights báo cáo")
+    uploaded_unit_report = st.file_uploader("Tải lên Báo cáo đơn vị", type=["docx", "txt"])
+    if uploaded_unit_report is not None:
+        if uploaded_unit_report.type == "text/plain":
+            st.session_state.unit_report_content = uploaded_unit_report.getvalue().decode("utf-8")
+        else:
+            report = Document(uploaded_unit_report)
+            st.session_state.unit_report_content = '\n'.join([para.text for para in report.paragraphs])
+
+    uploaded_unit_report_insights = st.file_uploader("Tải lên Insights báo cáo", type=["docx", "txt"])
+    if uploaded_unit_report_insights is not None:
+        if uploaded_unit_report_insights.type == "text/plain":
+            st.session_state.unit_report_insights_content = uploaded_unit_report_insights.getvalue().decode("utf-8")
+        else:
+            report = Document(uploaded_unit_report_insights)
+            st.session_state.unit_report_insights_content = '\n'.join([para.text for para in report.paragraphs])
+
+    if uploaded_unit_report is not None:
+        st.write("Báo cáo đã được tải lên thành công!")
+
+    if uploaded_unit_report_insights is not None:
+        st.write("Insights báo cáo đã được tải lên thành công!")
+
+    if st.button("Evaluate Unit Report Insights"):
+        if uploaded_unit_report is not None:
+            st.write("Đang đánh giá report insights...")
+            payload = {
+                "question": "So sánh các insights báo cáo đối với báo cáo đơn vị",
+                "overrideConfig": {
+                    "promptValues": {
+                        "unit_report": st.session_state.unit_report_content,
+                        "unit_report_insights": st.session_state.unit_report_insights_content
+                    }
+                }
+            }
+            response = requests.post(UNIT_INSIGHTS_EVALUATOR, json=payload)
+            try:
+                st.markdown(response.json()["text"])
+            except:
+                st.write(response.json())
+
+with tab2:
+    st.title("Tải lên báo cáo tổng hợp và đề cương báo cáo")
+    uploaded_summarized_report = st.file_uploader("Tải lên báo cáo tổng hợp", type=["docx", "txt"])
+    if uploaded_summarized_report is not None:
+        if uploaded_summarized_report.type == "text/plain":
+            st.session_state.summarized_report_content = uploaded_summarized_report.getvalue().decode("utf-8")
+        else:
+            report = Document(uploaded_summarized_report)
+            st.session_state.summarized_report_content = '\n'.join([para.text for para in report.paragraphs])
+
+    uploaded_outline = st.file_uploader("Tải lên Đề cương báo cáo", type=["docx", "txt"])
+    if uploaded_outline is not None:
+        if uploaded_outline.type == "text/plain":
+            st.session_state.outline_content = uploaded_outline.getvalue().decode("utf-8")
+        else:
+            outline = Document(uploaded_outline)
+            st.session_state.outline_content = '\n'.join([para.text for para in outline.paragraphs])
+        
+    if uploaded_summarized_report is not None:
+        st.write("Báo cáo đã được tải lên thành công!")
+
+    if uploaded_outline is not None:
+        st.write("Đề cương báo cáo đã được tải lên thành công!")
+
+    # Tạo nút để tải xuống file
+    if st.button("Evaluate Summarized Report"):
+        if uploaded_summarized_report is not None:
+            st.write("Đang đánh giá báo cáo...")
+            payload = {
+                "question": "So sánh nội dung báo cáo với đề cương báo cáo",
+                "overrideConfig": {
+                    "promptValues": {
+                        "report": st.session_state.summarized_report_content,
+                        "report_template": st.session_state.outline_content
+                    }
+                }
+            }
+            response = requests.post(SUMMARIZED_REPORT_EVALUATOR, json=payload)
+            try:
+                st.markdown(response.json()["text"])
+            except:
+                st.write(response.json())
